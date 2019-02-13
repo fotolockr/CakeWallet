7b794a1a6525ae0b88ea56fbeb9fbe524457b7e9
Cake <info@cakewallet.io>
2019-02-13 14:55:17 +0200

    Added build scripts

---
+++ CWMonero/install_osx_dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SOURCE_DIR=`pwd`
+EXTERNAL_DIR_PATH="$SOURCE_DIR/../SharedExternal"
+CPPZMQ_SOURCES_PATH="$EXTERNAL_DIR_PATH/cppzmq"
+CPPZMQ_HEADER_FILE="zmq.hpp"
+CPPZMQ_HEADER_PATH="/usr/local/include/$CPPZMQ_HEADER_FILE"
+
+function install_brew_package {
+  if brew ls --versions $1 > /dev/null; then 
+    echo "$1 is already installed!"
+  else
+    echo "Installing $1"
+    brew install $1
+  fi
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Check if brew is installed
+
+  install_brew_package "cmake"
+  install_brew_package "pkg-config"
+
+  if [ ! -f $CPPZMQ_HEADER_PATH ]; then
+      echo "File not found!"
+  
+    if [ ! -d $CPPZMQ_SOURCES_PATH ]; then
+      echo "Installing zeromq"
+      git clone https://github.com/zeromq/cppzmq $CPPZMQ_SOURCES_PATH
+    fi
+
+    cp $CPPZMQ_SOURCES_PATH/$CPPZMQ_HEADER_FILE $CPPZMQ_HEADER_PATH
+  fi
+ 
+  ./install-missing-headers.sh
+fi
